adb372c3e4363aa13ddc25afea46b1590b1332f8
omar <152991051+omartrjm@users.noreply.github.com>
2024-02-19 13:23:26 +0300

    u

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -7,9 +7,11 @@ using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
+using Constants = ServerLibrary.Helpers.Constants;
 
 namespace ServerLibrary.Repositories.Implementations
 {
@@ -30,6 +32,22 @@ namespace ServerLibrary.Repositories.Implementations
                     Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
                 });
 
+            // check, creat and assign role
+            var checkAdminRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.Admin));
+            if (checkAdminRole is null)
+            {
+                var createAdminRole = await AddToDatabase(new SystemRole() {Name = Constants.Admin });
+                await AddToDatabase(new UserRole() { RoleId = createAdminRole.Id, UserId = applicationUser.Id });
+                return new GeneralResponse(true,"Account created");
+            }
+
+            var checkUserRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.User));
+            SystemRole response = new();
+            if (checkUserRole is null) 
+            {
+                response 
+            }
+
         }   
 
         public Task<LoginResponse> SignInAsync(Login user)
@@ -39,5 +57,12 @@ namespace ServerLibrary.Repositories.Implementations
 
         private async Task<ApplicationUser> FindUserByEmail(string email) =>
             await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
+
+        private async Task<T> AddToDatabase<T>(T model)
+        {
+            var result = appDbContext.Add(model!);
+            await appDbContext.SaveChangesAsync();
+            return (T)result.Entity;
+        }
     }
 }
