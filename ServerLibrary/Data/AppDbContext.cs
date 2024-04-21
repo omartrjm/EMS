561c7d93465e5a31cf48673b45cd28e5088a11e2
omar <152991051@users.noreply.github.com>
2024-04-21 07:28:29 +0300

    update

--- ServerLibrary/Data/AppDbContext.cs
+++ ServerLibrary/Data/AppDbContext.cs
@@ -16,7 +16,7 @@ namespace ServerLibrary.Data
         public DbSet<Branch> Branches { get; set; }
 
         //Country /City /Town
-        public DbSet<Country> Countries  { get; set; }
+        public DbSet<Country> Countries  { get; set; } //
         public DbSet<City> Cities  { get; set; }
         public DbSet<Town> Towns  { get; set; }
 
